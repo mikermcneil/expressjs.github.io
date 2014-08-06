e96b3a9918eb786424eefc1e27f0a7fc0064c276
Mike McNeil <mike@balderdash.co>
2014-08-06 10:17:50 -0500

    Setup first pass at script to render modules.md

--- standalone/render-expressjs-modules/index.js
+++ standalone/render-expressjs-modules/index.js
@@ -2,11 +2,24 @@
  * Module dependencies
  */
 
+var path = require('path');
 var _ = require('lodash');
-
+var r_getWebFrameworkRepos = require('../get-expressjs-repos');
+var r_renderTpl = require('../render-template');
 
 module.exports = function(options, cb) {
-	require('../standalone/render-template')({
 
-	}, cb);
+  r_getWebFrameworkRepos({}, function(err, repos) {
+    if (err) return cb(err);
+
+    // console.log('META',require('../../modules.meta.json'));
+    r_renderTpl({
+      outputPath: path.resolve(process.cwd(), './MODULES.md'),
+      tplPath: path.resolve(__dirname, './.MODULES.tpl.md'),
+      locals: {
+        repos: repos
+      }
+    }, cb);
+
+  });
 };
